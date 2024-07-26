ec33c3645c191e4daf5574c4e4c0358c9812d99b
SethupathyD <sethupathy.d@syncfusion.com>
2024-07-26 12:47:29 +0530

    Code updated

---
+++ ThemeSample/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace ThemeSample
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Application.Current.UserAppTheme = AppTheme.Dark;
+        }
+    }
+
+}
